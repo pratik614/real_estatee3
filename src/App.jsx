9e0648356cc1581944e7cbf69be90154359e4f58
pratik <acharyapratik761@gmail.com>
2023-11-18 20:11:55 +0545

    creating pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className='flex items-center justify-center'>
-      this is app
-    </div>
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/profile' element={<Profile />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/register' element={<Register />}/>
+
+    </Routes>
+  </Router>
   )
 }
 
